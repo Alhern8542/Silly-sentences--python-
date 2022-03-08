3bc28378d9d2e0e3f39809218e0892bd332e6243
Al Hern <Alhern8542@gmail.com>
2022-03-08 01:02:00 -0600

    first commit

---
+++ silly.py
@@ -0,0 +1,31 @@
+import random
+import words
+
+
+def silly_string(nouns, verbs, templates):
+    # Choose a random template.
+    template = random.choice(templates)
+
+    # We'll append strings into this list for output.
+    output = []
+
+    # Keep track of where in the template string we are.
+    index = 0
+
+    # Add a while loop here.
+    while index < len(template):
+        if template[index:index+len('{{noun}}')] == '{{noun}}':
+            index += len('{{noun}}')
+            output.append(random.choice(nouns))
+        elif template[index:index+len('{{verb}}')] == '{{verb}}':
+            index += len('{{verb}}')
+            output.append(random.choice(verbs))
+        else:
+            output.append(template[index])
+            index +=1
+
+    # After the loop has finished, join the output and return it.
+    return "".join(output)
+
+# To see the results, we need to call the funtion and print what it returns:
+print(silly_string(words.nouns, words.verbs, words.templates))
